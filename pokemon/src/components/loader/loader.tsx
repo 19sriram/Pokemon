5f975f618a8e33175e51484e616dc861cae412ea
sram <sram@srams-MacBook-Air.local>
2021-08-09 09:35:56 +0530

    loader change

--- pokemon/src/components/loader/loader.tsx
+++ pokemon/src/components/loader/loader.tsx
@@ -3,8 +3,10 @@ import "./loader.scss";
 export default function Loader() {
   return (
     <div className="center-on-page">
+       <h2>Loading...</h2>
       <div className="pokeball">
         <div className="pokeball__button"></div>
+       
       </div>
     </div>
   );
